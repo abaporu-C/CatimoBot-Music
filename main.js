0f47ab98421deded4bf4782a8ed521d3b93aac32
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-02-26 00:36:08 -0500

    Minor grammar correction

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//Importing libraries
+//Utilities
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 const {prefix, token} = require('./config.json');
-
-//global Variables
 client.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('./commands');
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
