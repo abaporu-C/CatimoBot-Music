ed392cebeab80e7d9f486841a1f95aabdb6a0232
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-02-26 00:24:23 -0500

    Event handler implemented

---
+++ events/message.js
@@ -0,0 +1,31 @@
+module.exports = {
+	name: 'message',
+	execute(message, client) {
+        const {prefix} = require('../config.json');
+		if (message.member.user.bot || !message.content.startsWith(prefix)) return;
+
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        if (!client.commands.has(commandName)) return;
+
+        const command = client.commands.get(commandName);
+
+        if (command.args && !args.length) {
+            let reply = `You didn't provide any arguments, ${message.author}!`;
+
+            if (command.usage) {
+                reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+            }
+
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+
+        try {
+	        command.execute(message, args);
+        } catch (error) {
+	        console.error(error);
+	        message.reply('there was an error trying to execute that command!');
+        }
+	},
+};
--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 const {prefix, token} = require('./config.json');
 
 //global Variables
@@ -10,6 +11,15 @@ const commandFolders = fs.readdirSync('./commands');
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
 
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
 for (const folder of commandFolders) {
 	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
@@ -17,40 +27,6 @@ for (const folder of commandFolders) {
 		client.commands.set(command.name, command);
 	}
 }
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-
-client.on('message', message => {
-    if (message.member.user.bot || !message.content.startsWith(prefix)) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    if (!client.commands.has(commandName)) return;
-
-    const command = client.commands.get(commandName);
-
-    if (command.args && !args.length) {
-        let reply = `You didn't provide any arguments, ${message.author}!`;
-
-        if (command.usage) {
-            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-        }
-
-        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-    }
-
-    try {
-	    command.execute(message, args);
-    } catch (error) {
-	    console.error(error);
-	    message.reply('there was an error trying to execute that command!');
-    }
-});
 
 
 client.login(token);
