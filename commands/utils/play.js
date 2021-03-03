c8c1a0d7a6050ea67b13050f928a916f8772f7c6
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-03-02 23:33:54 -0500

    Fixed drop command

--- commands/utils/play.js
+++ commands/utils/play.js
@@ -136,12 +136,14 @@ const list_queue = (message, server_queue) => {
 }
 
 const drop_song = (message, args, server_queue) => {
-    if(!args){
+    if(!args[0]){
         message.channel.send('You have to call this command with a song number');
     } else if(isNaN(parseInt(args[0])) || parseInt(args[0]) < 1) {
-        message.channel.send('You have to enter a NUMBER bigger than zero to drop a song in the list.');
+        message.channel.send('You have to enter a NUMBER bigger than zero to drop a song of the list.');
     } else if(parseInt(args[0]) > server_queue.songs.length) {
         message.channel.send('There is no such number in the list.')
+    } else if(parseInt(args[0]) === 1){
+        skip_song(message, server_queue);
     } else {
         newQueue = [];
         for(let i = 0; i < server_queue.songs.length; i++){
