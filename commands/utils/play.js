517244379d964b491693629b25fa3ad0f5c3282c
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-03-02 23:21:38 -0500

    Drop command added

--- commands/utils/play.js
+++ commands/utils/play.js
@@ -5,7 +5,7 @@ const queue = new Map();
 
 module.exports = {
     name: 'play',
-    aliases: ['skip', 'stop', 'list'], 
+    aliases: ['skip', 'stop', 'list', 'drop'], 
     cooldown: 0,
     description: 'Advanced music bot',
     async execute(message, args, cmd, client){
@@ -84,6 +84,9 @@ module.exports = {
         else if(cmd === 'list') {
             list_queue(message, server_queue);
         }
+        else if(cmd === 'drop') {
+            drop_song(message, args, server_queue)
+        }
     }
     
 }
@@ -129,5 +132,23 @@ const list_queue = (message, server_queue) => {
         message.channel.send(songList);
     } else{
         message.channel.send("There is no queue yet! Use the <^play> command to add one song to the song queue!")
+    }
+}
+
+const drop_song = (message, args, server_queue) => {
+    if(!args){
+        message.channel.send('You have to call this command with a song number');
+    } else if(isNaN(parseInt(args[0])) || parseInt(args[0]) < 1) {
+        message.channel.send('You have to enter a NUMBER bigger than zero to drop a song in the list.');
+    } else if(parseInt(args[0]) > server_queue.songs.length) {
+        message.channel.send('There is no such number in the list.')
+    } else {
+        newQueue = [];
+        for(let i = 0; i < server_queue.songs.length; i++){
+            if(parseInt(args[0]) - 1 != i) {
+                newQueue.push(server_queue.songs[i])
+            }
+        }
+        server_queue.songs = newQueue;
     }
 }
