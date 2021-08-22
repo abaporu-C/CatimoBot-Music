a1e7549ed30f0eeca9957963687f3d8b22dfb226
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-08-21 21:06:56 -0400

    Updated discord.js v12

--- commands/Play/aliases/stop.js
+++ commands/Play/aliases/stop.js
@@ -1,5 +1,8 @@
 module.exports =  (message, server_queue) => {
     if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
-    server_queue.songs = [];
-    server_queue.connection.dispatcher.end();
+    if (server_queue.connection.dispatcher){
+        server_queue.songs = [];
+        server_queue.connection.dispatcher.destroy();
+    }
+    else server_queue.connection.disconnect();
 }
